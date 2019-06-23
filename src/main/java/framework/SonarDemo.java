96f58365e7b5e4d43ec9fbda0561bb709c75632d
saurav <sauravpd58@gmail.com>
2019-06-23 14:26:49 +0530

    commit changes in SonarDemo

--- src/main/java/framework/SonarDemo.java
+++ src/main/java/framework/SonarDemo.java
@@ -1,7 +1,10 @@
 package framework;
 
+import org.apache.log4j.Logger;
+
 public class SonarDemo 
 {
+	public static Logger LOG=Logger.getLogger(SonarDemo.class);
 	public static void main(String[] args) 
 	{
 		System.out.println("SonarQube Jenkins Integation Demo .. !!");
