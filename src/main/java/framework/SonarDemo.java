38413a23d3a1f987fe67b5b021cbab9e45b6788d
saurav <sauravpd58@gmail.com>
2019-06-23 17:01:47 +0530

    changed this public static logger field to final

--- src/main/java/framework/SonarDemo.java
+++ src/main/java/framework/SonarDemo.java
@@ -4,7 +4,7 @@ import org.apache.log4j.Logger;
 
 public class SonarDemo 
 {
-	public static Logger logger=Logger.getLogger(SonarDemo.class);
+	public static final Logger logger=Logger.getLogger(SonarDemo.class);
 	public static void main(String[] args) 
 	{
 		logger.info("SonarQube Jenkins Integation Demo .. !!");
